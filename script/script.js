25b18129d0df5b19e2ebaf4412ced32761ff4bea
Th3freeman <thefrman345@gmail.com>
2019-10-31 02:17:59 -0400

    added script to create divs to display the search results which it does (you can see if you inspect the page) but for some reason the displays are completely hidden

--- script/script.js
+++ script/script.js
@@ -29,10 +29,53 @@ $("#searchButton").on("click", function (event) {
     })
         .then(function (response) {
 
-            console.log(queryURL);
+            console.log(response)
+            // Creating a div to hold the movie
+            var weatherDiv = $("#weatherShow").append(
+                $("<div class='forecast'>")
+                );
 
-            console.log(response);
+            // Storing the Temperature data
+            var Temperature = response.main;
+
+            console.log(Temperature);
+            // Creating an element to have the Temperature displayed
+            var p1 = $("<p>").text("Temperature: " + Temperature);
+
+            // Displaying the Temperature
+            weatherDiv.append(p1);
+
+            // // Storing the release year
+            // var released = response.Released;
+
+            // // Creating an element to hold the release year
+            // var p2 = $("<p>").text("Released: " + released);
+
+            // // Displaying the release year
+            // weatherDiv.append(p2);
+
+            // // Storing the plot
+            // var plot = response.Plot;
+
+            // // Creating an element to hold the plot
+            // var pThree = $("<p>").text("Plot: " + plot);
+
+            // // Appending the plot
+            // weatherDiv.append(pThree);
+
+            // // Retrieving the URL for the image
+            // var imgURL = response.Poster;
+
+            // // Creating an element to hold the image
+            // var image = $("<img>").attr("src", imgURL);
+
+            // Appending the image
+            // weatherDiv.append(image);
+
+            // Putting the entire movie above the previous movies
+            $("#weatherShow").prepend(weatherDiv);
         });
+
 });
 
 

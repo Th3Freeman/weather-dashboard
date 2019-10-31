de0350811bc1064beeaeccab92e10fe137146dca
Th3freeman <thefrman345@gmail.com>
2019-10-31 10:13:27 -0400

    tweaked the url and it finally shows the correct value that i want output

--- script/script.js
+++ script/script.js
@@ -1,8 +1,6 @@
 var searched
 var apiKey = "6e27f67d5b1eed622cee73e1d80b3160";
 
-$("#searchBar").val().trim(); + "&units=imperial&appid" + apiKey;
-
 
 function weatherSearch() {
     $.ajax({
@@ -22,7 +20,7 @@ function weatherSearch() {
 $("#searchButton").on("click", function (event) {
     event.preventDefault();
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
-        $("#searchBar").val().trim() + "&units=imperial&appid=" + apiKey;
+        $("#searchBar").val().trim() + "&units=imperial&"+"appid=" + apiKey;
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -30,19 +28,16 @@ $("#searchButton").on("click", function (event) {
         .then(function (response) {
 
             console.log(response)
-            // Creating a div to hold the response
             var weatherDiv = $("#weatherShow").append(
                 $("<div class='forecast'>")
                 );
 
-            // Storing the Temperature data
-            var Temperature = response.main;
+            var Temperature = response.main.temp;
 
             console.log(Temperature);
-            // Creating an element to have the Temperature displayed
+
             var p1 = $("<p>").text("Temperature: " + Temperature);
 
-            // Displaying the Temperature
             weatherDiv.append(p1);
 
             $("#weatherShow").prepend(weatherDiv);

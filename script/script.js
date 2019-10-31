deb16eefeb1da6ca1eef3bdd142e8a0c3be6d789
Th3freeman <thefrman345@gmail.com>
2019-10-31 01:24:07 -0400

    got the search bar functioning properly, had to use a click handler as it wouldnt read the form when the button used an 'onclick'

--- script/script.js
+++ script/script.js
@@ -1,26 +1,39 @@
+var searched
 var apiKey = "6e27f67d5b1eed622cee73e1d80b3160";
-// var searched = $("#searchBar").val().trim()
-// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
-//     $("#searchBar").val().trim() + "&units=imperial&appid" + apiKey;
 
-$("#searchButton").on("click", function (event) {
-    event.preventDefault();
-    var weather = $("#searchBar").val().trim()
-    console.log(weather);
-});
+$("#searchBar").val().trim(); + "&units=imperial&appid" + apiKey;
+
+
 function weatherSearch() {
-    
     $.ajax({
         url: queryURL,
         method: "GET"
     })
-         .then(function (response) {
+        .then(function (response) {
 
-             console.log(queryURL);
+            console.log(queryURL);
 
             console.log(response);
         });
-        console.log(searched)
+    console.log(searched)
 };
+
+
+$("#searchButton").on("click", function (event) {
+    event.preventDefault();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +
+        $("#searchBar").val().trim() + "&units=imperial&appid=" + apiKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+
+            console.log(queryURL);
+
+            console.log(response);
+        });
+});
+
 
 
